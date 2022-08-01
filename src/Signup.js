b01560d26f13385a152f8fd87051f19770845a52
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 08:34:06 +0530

    signup added

--- src/Signup.js
+++ src/Signup.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 function Signup() {
-  const state = useSelector((s => s));
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  const state = useSelector((s) => s);
   const dispatch = useDispatch();
+  const onSubmit = (data) => {
+    let condition = state.users.some((x) => x.email === data.email); //true
+    if (condition) {
+      toast('user already exists try with other credentials');
+    } else {
+      toast('signup successfull');
+      navigate('/Login');
+    }
+  };
   return (
     <div>
       <h1>Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="name" {...register('name', { required: true })} />
+        {errors.name && <p className="err">name required</p>}
+
+        <input placeholder="email" {...register('email', { required: true })} />
+        {errors.email && <p className="err">email required</p>}
+
+        <input placeholder="phone" {...register('phone', { required: true })} />
+        {errors.phone && <p className="err">phone required</p>}
+
+        <input
+          placeholder="password"
+          {...register('password', { required: true })}
+        />
+        {errors.password && <p className="err">password required</p>}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 }
